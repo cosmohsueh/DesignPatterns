74ac02b8b4e795c703ae4406d8d4a6eed6af50e4
cosmo <cosmo.hsueh@gmail.com>
2016-04-29 09:34:26 +0800

    解釋器模式

---
+++ DesignPatterns/src/main/java/com/cosmo/InterpreterPattern/Expression.java
@@ -0,0 +1,8 @@
+package com.cosmo.InterpreterPattern;
+
+import java.util.HashMap;
+
+public abstract class Expression {
+
+	public abstract int interpreter(HashMap<String, Integer> var);
+}
---
+++ DesignPatterns/src/main/java/com/cosmo/InterpreterPattern/SymbolExpression.java
@@ -0,0 +1,13 @@
+package com.cosmo.InterpreterPattern;
+
+public abstract class SymbolExpression extends Expression {
+
+	protected Expression left;
+	protected Expression right;
+
+	public SymbolExpression(Expression left, Expression right) {
+		this.left = left;
+		this.right = right;
+	}
+
+}
